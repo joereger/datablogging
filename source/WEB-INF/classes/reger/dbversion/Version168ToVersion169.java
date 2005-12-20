9e7cc10b5fb651d176e6742dc82bccbff5835bd2
joereger <joereger@d42e49d7-e3ba-4d4b-9865-d14956a61029>
2005-12-20 17:03:13 +0000

    Creating a stub in Version168ToVersion169 for Pawan's next assignment.

---
+++ source/WEB-INF/classes/reger/dbversion/Version168ToVersion169.java
@@ -0,0 +1,65 @@
+package reger.dbversion;
+
+import reger.core.db.Db;
+import reger.core.dbupgrade.UpgradeDatabaseOneVersion;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+
+/**
+ * This creates the base database if none exists.
+ */
+public class Version168ToVersion169 implements UpgradeDatabaseOneVersion{
+
+
+    public void doUpgrade(){
+
+        //Pawan's first database update
+
+
+
+
+
+    }
+
+
+//Sample sql statements
+
+    //-----------------------------------
+    //-----------------------------------
+    //int count = Db.RunSQLUpdate("CREATE TABLE `pltemplate` (`pltemplateid` int(11) NOT NULL auto_increment, logid int(11), plid int(11), type int(11), templateid int(11), PRIMARY KEY  (`pltemplateid`)) ENGINE=MyISAM DEFAULT CHARSET=latin1;");
+    //-----------------------------------
+    //-----------------------------------
+
+    //-----------------------------------
+    //-----------------------------------
+    //int count = Db.RunSQLUpdate("ALTER TABLE megachart CHANGE daterangesavedsearchid daterangesavedsearchid int(11) NOT NULL default '0'");
+    //-----------------------------------
+    //-----------------------------------
+
+    //-----------------------------------
+    //-----------------------------------
+    //int count = Db.RunSQLUpdate("ALTER TABLE account DROP gps");
+    //-----------------------------------
+    //-----------------------------------
+
+    //-----------------------------------
+    //-----------------------------------
+    //int count = Db.RunSQLUpdate("ALTER TABLE megalogtype ADD isprivate int(11) NOT NULL default '0'");
+    //-----------------------------------
+    //-----------------------------------
+
+    //-----------------------------------
+    //-----------------------------------
+    //int count = Db.RunSQLUpdate("DROP TABLE megafielduser");
+    //-----------------------------------
+    //-----------------------------------
+
+    //-----------------------------------
+    //-----------------------------------
+    //int count = Db.RunSQLUpdate("CREATE INDEX name_of_index ON table (field1, field2)");
+    //-----------------------------------
+    //-----------------------------------
+
+
+}
