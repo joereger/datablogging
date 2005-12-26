da935c7bb1e9403085dea7688964c14cbe4f8b9d
joereger <joereger@d42e49d7-e3ba-4d4b-9865-d14956a61029>
2005-12-26 16:28:43 +0000

    Updating template image paths.  Quick PrivateLabel.java bug fix on new pl's.

--- source/WEB-INF/classes/reger/template/AllOneClickTemplateSets.java
+++ source/WEB-INF/classes/reger/template/AllOneClickTemplateSets.java
@@ -22,7 +22,7 @@ public class AllOneClickTemplateSets {
         allOneClickTemplateSets.put(i++, new OneClickTemplateSet(i, -1, -101, 0, "Beautiful", WebAppRootDir.getWebAppRootPath()+"templates/site/beautiful/images/thumbnail.jpg"));
         allOneClickTemplateSets.put(i++, new OneClickTemplateSet(i, -2, -102, 0, "Branches", WebAppRootDir.getWebAppRootPath()+"templates/site/branches/images/thumbnail.jpg"));
         allOneClickTemplateSets.put(i++, new OneClickTemplateSet(i, -3, -103, 0, "Gothic Trees", WebAppRootDir.getWebAppRootPath()+"templates/site/gothictrees/images/thumbnail.jpg"));
-        allOneClickTemplateSets.put(i++, new OneClickTemplateSet(i, -4, -104, 0, "Nigglish", WebAppRootDir.getWebAppRootPath()+"templates/site/nigglish/images/thumbnail.jpg"));
+        allOneClickTemplateSets.put(i++, new OneClickTemplateSet(i, -4, -104, 0, "nigglish", WebAppRootDir.getWebAppRootPath()+"templates/site/nigglish/images/thumbnail.jpg"));
         allOneClickTemplateSets.put(i++, new OneClickTemplateSet(i, -5, -105, 0, "Spanish Vacation", WebAppRootDir.getWebAppRootPath()+"templates/site/spanishvacation/images/thumbnail.jpg"));
 
     }
