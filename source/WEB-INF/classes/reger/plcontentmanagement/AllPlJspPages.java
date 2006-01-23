e8cda4e7829e876168a8eff5d5bcab5681713af8
plingala <plingala@d42e49d7-e3ba-4d4b-9865-d14956a61029>
2006-01-23 23:24:52 +0000

    Community Tags link Added

--- source/WEB-INF/classes/reger/plcontentmanagement/AllPlJspPages.java
+++ source/WEB-INF/classes/reger/plcontentmanagement/AllPlJspPages.java
@@ -36,6 +36,7 @@ public class AllPlJspPages {
         nestedNavItems.add((NestedNavItem)(new PlJspPage(-45, "Log Types", "community-logtypes.log", NestedNavItem.NESTEDNAVTYPEPLJSPPAGE, -3, 6)));
         nestedNavItems.add((NestedNavItem)(new PlJspPage(-46, "Tell a Friend", "community-tellfriend.log", NestedNavItem.NESTEDNAVTYPEPLJSPPAGE, -3, 7)));
         nestedNavItems.add((NestedNavItem)(new PlJspPage(-47, "Discussion Forum", "community-discuss.log", NestedNavItem.NESTEDNAVTYPEPLJSPPAGE, -3, 8)));
+        nestedNavItems.add((NestedNavItem)(new PlJspPage(-53, "Community Tags", "community-tags.log", NestedNavItem.NESTEDNAVTYPEPLJSPPAGE, -3, 9)));
 
 
         //More Info
