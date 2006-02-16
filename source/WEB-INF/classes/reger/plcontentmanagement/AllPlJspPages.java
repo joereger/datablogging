44e695bdc93fdccd30c8a4cff5ba959fbee608cc
joereger <joereger@d42e49d7-e3ba-4d4b-9865-d14956a61029>
2006-02-16 13:52:38 +0000

    Merging Duncan's changes from the /general branch version 418 into the /trunk

--- source/WEB-INF/classes/reger/plcontentmanagement/AllPlJspPages.java
+++ source/WEB-INF/classes/reger/plcontentmanagement/AllPlJspPages.java
@@ -20,47 +20,54 @@ public class AllPlJspPages {
 
         //Top Level
         nestedNavItems.add((NestedNavItem)(new PlJspPage(-1, "Home", "index.log", NestedNavItem.NESTEDNAVITEMBASE, 0, 1)));
-        nestedNavItems.add((NestedNavItem)(new PlJspPage(-2, "Get Started", "signup.log", NestedNavItem.NESTEDNAVITEMBASE, 0, 2)));
-        nestedNavItems.add((NestedNavItem)(new PlJspPage(-3, "Community", "community.log", NestedNavItem.NESTEDNAVITEMBASE, 0, 3)));
-        nestedNavItems.add((NestedNavItem)(new PlJspPage(-4, "More Info", "moreinfo.log", NestedNavItem.NESTEDNAVITEMBASE, 0, 4)));
+        nestedNavItems.add((NestedNavItem)(new PlJspPage(-2, "Sign Up - It's Free!", "signup.log", NestedNavItem.NESTEDNAVITEMBASE, 0, 2)));
+        nestedNavItems.add((NestedNavItem)(new PlJspPage(-46, "Tell a Friend!", "community-tellfriend.log", NestedNavItem.NESTEDNAVTYPEPLJSPPAGE, -2, 1)));
+        nestedNavItems.add((NestedNavItem)(new PlJspPage(-3, "See Our Community", "community.log", NestedNavItem.NESTEDNAVITEMBASE, 0, 3)));
+        nestedNavItems.add((NestedNavItem)(new PlJspPage(-4, "Want to Learn More?", "moreinfo.log", NestedNavItem.NESTEDNAVITEMBASE, 0, 4)));
+
 
 
 
 
         //Community
-        nestedNavItems.add((NestedNavItem)(new PlJspPage(-40, "Blog Sites", "community-sites.log", NestedNavItem.NESTEDNAVTYPEPLJSPPAGE, -3, 1)));
-        nestedNavItems.add((NestedNavItem)(new PlJspPage(-41, "Entries", "community-entries.log", NestedNavItem.NESTEDNAVTYPEPLJSPPAGE, -3, 2)));
+        nestedNavItems.add((NestedNavItem)(new PlJspPage(-40, "Datablog Sites", "community-sites.log", NestedNavItem.NESTEDNAVTYPEPLJSPPAGE, -3, 1)));
+        nestedNavItems.add((NestedNavItem)(new PlJspPage(-41, "Log Entries", "community-entries.log", NestedNavItem.NESTEDNAVTYPEPLJSPPAGE, -3, 2)));
         nestedNavItems.add((NestedNavItem)(new PlJspPage(-42, "Entries by Log Type", "community-entries-logtypes.log", NestedNavItem.NESTEDNAVTYPEPLJSPPAGE, -3, 3)));
-        nestedNavItems.add((NestedNavItem)(new PlJspPage(-43, "Find a Blog Site", "community-findsite.log", NestedNavItem.NESTEDNAVTYPEPLJSPPAGE, -3, 4)));
-        nestedNavItems.add((NestedNavItem)(new PlJspPage(-44, "Search", "community-search.log", NestedNavItem.NESTEDNAVTYPEPLJSPPAGE, -3, 5)));
-        nestedNavItems.add((NestedNavItem)(new PlJspPage(-45, "Log Types", "community-logtypes.log", NestedNavItem.NESTEDNAVTYPEPLJSPPAGE, -3, 6)));
-        nestedNavItems.add((NestedNavItem)(new PlJspPage(-46, "Tell a Friend", "community-tellfriend.log", NestedNavItem.NESTEDNAVTYPEPLJSPPAGE, -3, 7)));
+        nestedNavItems.add((NestedNavItem)(new PlJspPage(-53, "User Data Tags", "community-tags.log", NestedNavItem.NESTEDNAVTYPEPLJSPPAGE, -3, 4)));
+        nestedNavItems.add((NestedNavItem)(new PlJspPage(-43, "Find a Datablog Site", "community-findsite.log", NestedNavItem.NESTEDNAVTYPEPLJSPPAGE, -3, 5)));
+        nestedNavItems.add((NestedNavItem)(new PlJspPage(-44, "Complex Search", "community-search.log", NestedNavItem.NESTEDNAVTYPEPLJSPPAGE, -3, 6)));
+        nestedNavItems.add((NestedNavItem)(new PlJspPage(-55, "Locate a User Group", "community-groups.log", NestedNavItem.NESTEDNAVTYPEPLJSPPAGE, -3, 7)));
         nestedNavItems.add((NestedNavItem)(new PlJspPage(-47, "Discussion Forum", "community-discuss.log", NestedNavItem.NESTEDNAVTYPEPLJSPPAGE, -3, 8)));
-        nestedNavItems.add((NestedNavItem)(new PlJspPage(-53, "Tags", "community-tags.log", NestedNavItem.NESTEDNAVTYPEPLJSPPAGE, -3, 9)));
-        nestedNavItems.add((NestedNavItem)(new PlJspPage(-55, "Groups", "community-groups.log", NestedNavItem.NESTEDNAVTYPEPLJSPPAGE, -3, 10)));
+
+
+
+
+
 
 
         //More Info
-        nestedNavItems.add((NestedNavItem)(new PlJspPage(-23, "Personal Use", "datablogging-people.log", NestedNavItem.NESTEDNAVTYPEPLJSPPAGE, -4, 1)));
-        nestedNavItems.add((NestedNavItem)(new PlJspPage(-24, "Business Use", "datablogging-business.log", NestedNavItem.NESTEDNAVTYPEPLJSPPAGE, -4, 2)));
-        nestedNavItems.add((NestedNavItem)(new PlJspPage(-20, "Tours", "tour.log", NestedNavItem.NESTEDNAVTYPEPLJSPPAGE, -4, 3)));
-        nestedNavItems.add((NestedNavItem)(new PlJspPage(-21, "Features", "features.log", NestedNavItem.NESTEDNAVTYPEPLJSPPAGE, -4, 4)));
-        nestedNavItems.add((NestedNavItem)(new PlJspPage(-22, "What is datablogging?", "what-is-datablogging.log", NestedNavItem.NESTEDNAVTYPEPLJSPPAGE, -4, 5)));
-        nestedNavItems.add((NestedNavItem)(new PlJspPage(-25, "Version History", "versions.log", NestedNavItem.NESTEDNAVTYPEPLJSPPAGE, -4, 6)));
-        nestedNavItems.add((NestedNavItem)(new PlJspPage(-26, "Downloadable Version", "download.log", NestedNavItem.NESTEDNAVTYPEPLJSPPAGE, -4, 7)));
-        nestedNavItems.add((NestedNavItem)(new PlJspPage(-27, "Blogging Overview", "tour-overview-01.log", NestedNavItem.NESTEDNAVTYPEPLJSPPAGE, -4, 8)));
+        nestedNavItems.add((NestedNavItem)(new PlJspPage(-20, "Take A Tour", "tour.log", NestedNavItem.NESTEDNAVTYPEPLJSPPAGE, -4, 1)));
+        nestedNavItems.add((NestedNavItem)(new PlJspPage(-27, "Blogging Overview", "tour-overview-01.log", NestedNavItem.NESTEDNAVTYPEPLJSPPAGE, -4, 2)));
+        nestedNavItems.add((NestedNavItem)(new PlJspPage(-22, "What is datablogging?", "what-is-datablogging.log", NestedNavItem.NESTEDNAVTYPEPLJSPPAGE, -4, 3)));
+        nestedNavItems.add((NestedNavItem)(new PlJspPage(-21, "Our Features", "features.log", NestedNavItem.NESTEDNAVTYPEPLJSPPAGE, -4, 4)));
+        nestedNavItems.add((NestedNavItem)(new PlJspPage(-23, "Personal Use", "datablogging-people.log", NestedNavItem.NESTEDNAVTYPEPLJSPPAGE, -4, 5)));
+        nestedNavItems.add((NestedNavItem)(new PlJspPage(-24, "Business Use", "datablogging-business.log", NestedNavItem.NESTEDNAVTYPEPLJSPPAGE, -4, 6)));
+        nestedNavItems.add((NestedNavItem)(new PlJspPage(-26, "Enterprise Edition", "download.log", NestedNavItem.NESTEDNAVTYPEPLJSPPAGE, -4, 7)));
+        nestedNavItems.add((NestedNavItem)(new PlJspPage(-25, "Platform Versions", "versions.log", NestedNavItem.NESTEDNAVTYPEPLJSPPAGE, -4, 8)));
         nestedNavItems.add((NestedNavItem)(new PlJspPage(-28, "About Us", "what-is-reger.log", NestedNavItem.NESTEDNAVTYPEPLJSPPAGE, -4, 9)));
 
+
         //What is datablogging?
-        nestedNavItems.add((NestedNavItem)(new PlJspPage(-10, "Graphs", "what-is-datablogging-graphs.log", NestedNavItem.NESTEDNAVTYPEPLJSPPAGE, -22, 1)));
-        nestedNavItems.add((NestedNavItem)(new PlJspPage(-11, "Graph and Search", "what-is-datablogging-graphandsearch.log", NestedNavItem.NESTEDNAVTYPEPLJSPPAGE, -22, 2)));
-        nestedNavItems.add((NestedNavItem)(new PlJspPage(-12, "Adding Fields", "what-is-datablogging-newfield.log", NestedNavItem.NESTEDNAVTYPEPLJSPPAGE, -22, 3)));
-        nestedNavItems.add((NestedNavItem)(new PlJspPage(-13, "Structured Blogging", "what-is-datablogging-preliminary-structured-blogging-support.log", NestedNavItem.NESTEDNAVTYPEPLJSPPAGE, -22, 4)));
+        nestedNavItems.add((NestedNavItem)(new PlJspPage(-45, "Log Types", "community-logtypes.log", NestedNavItem.NESTEDNAVTYPEPLJSPPAGE, -22, 1)));
+        nestedNavItems.add((NestedNavItem)(new PlJspPage(-10, "Custom Data Graphs", "what-is-datablogging-graphs.log", NestedNavItem.NESTEDNAVTYPEPLJSPPAGE, -22, 2)));
+        nestedNavItems.add((NestedNavItem)(new PlJspPage(-11, "Advanced Queries", "what-is-datablogging-graphandsearch.log", NestedNavItem.NESTEDNAVTYPEPLJSPPAGE, -22, 3)));
+        nestedNavItems.add((NestedNavItem)(new PlJspPage(-12, "Custom Log Types", "what-is-datablogging-newfield.log", NestedNavItem.NESTEDNAVTYPEPLJSPPAGE, -22, 4)));
         nestedNavItems.add((NestedNavItem)(new PlJspPage(-14, "RSS Support", "what-is-datablogging-rss.log", NestedNavItem.NESTEDNAVTYPEPLJSPPAGE, -22, 5)));
         nestedNavItems.add((NestedNavItem)(new PlJspPage(-15, "RSS Embedding", "what-is-datablogging-rss-in-html.log", NestedNavItem.NESTEDNAVTYPEPLJSPPAGE, -22, 6)));
-        nestedNavItems.add((NestedNavItem)(new PlJspPage(-16, "Data Searching", "what-is-datablogging-search.log", NestedNavItem.NESTEDNAVTYPEPLJSPPAGE, -22, 7)));
-        nestedNavItems.add((NestedNavItem)(new PlJspPage(-17, "Data Portability", "what-is-datablogging-statement-on-data-portability.log", NestedNavItem.NESTEDNAVTYPEPLJSPPAGE, -22, 8)));
+        nestedNavItems.add((NestedNavItem)(new PlJspPage(-16, "Data Mining", "what-is-datablogging-search.log", NestedNavItem.NESTEDNAVTYPEPLJSPPAGE, -22, 7)));
+        nestedNavItems.add((NestedNavItem)(new PlJspPage(-17, "Data Porting", "what-is-datablogging-statement-on-data-portability.log", NestedNavItem.NESTEDNAVTYPEPLJSPPAGE, -22, 8)));
         nestedNavItems.add((NestedNavItem)(new PlJspPage(-18, "XML Schema", "what-is-datablogging-xml-schema.log", NestedNavItem.NESTEDNAVTYPEPLJSPPAGE, -22, 9)));
+            nestedNavItems.add((NestedNavItem)(new PlJspPage(-13, "Structured Blogging", "what-is-datablogging-preliminary-structured-blogging-support.log", NestedNavItem.NESTEDNAVTYPEPLJSPPAGE, -22, 10)));
 
 
 
@@ -74,10 +81,13 @@ public class AllPlJspPages {
 
 
         //About Us
-        nestedNavItems.add((NestedNavItem)(new PlJspPage(-50, "Contact Us", "biz-contactus.log", NestedNavItem.NESTEDNAVTYPEPLJSPPAGE, -28, 1)));
-        nestedNavItems.add((NestedNavItem)(new PlJspPage(-50, "Technology", "biz-technology.log", NestedNavItem.NESTEDNAVTYPEPLJSPPAGE, -28, 2)));
-        nestedNavItems.add((NestedNavItem)(new PlJspPage(-51, "Press", "biz-pr-overview.log", NestedNavItem.NESTEDNAVTYPEPLJSPPAGE, -28, 3)));
-        nestedNavItems.add((NestedNavItem)(new PlJspPage(-52, "Investors", "biz-investors.log", NestedNavItem.NESTEDNAVTYPEPLJSPPAGE, -28, 4)));
+        nestedNavItems.add((NestedNavItem)(new PlJspPage(-60, "Our Team", "biz-management.log", NestedNavItem.NESTEDNAVTYPEPLJSPPAGE, -28, 1)));
+        nestedNavItems.add((NestedNavItem)(new PlJspPage(-61, "Our Values", "biz-core-values.log", NestedNavItem.NESTEDNAVTYPEPLJSPPAGE, -28, 2)));
+        nestedNavItems.add((NestedNavItem)(new PlJspPage(-61, "Our Partners", "biz-partners.log", NestedNavItem.NESTEDNAVTYPEPLJSPPAGE, -28, 3)));
+        nestedNavItems.add((NestedNavItem)(new PlJspPage(-50, "Our Technology", "biz-technology.log", NestedNavItem.NESTEDNAVTYPEPLJSPPAGE, -28, 4)));
+        nestedNavItems.add((NestedNavItem)(new PlJspPage(-50, "Contact Us", "biz-contactus.log", NestedNavItem.NESTEDNAVTYPEPLJSPPAGE, -28, 5)));
+        nestedNavItems.add((NestedNavItem)(new PlJspPage(-51, "Press", "biz-pr-overview.log", NestedNavItem.NESTEDNAVTYPEPLJSPPAGE, -28, 6)));
+        nestedNavItems.add((NestedNavItem)(new PlJspPage(-52, "Investors", "biz-investors.log", NestedNavItem.NESTEDNAVTYPEPLJSPPAGE, -28, 7)));
 
 
 
