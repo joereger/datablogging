2dd959935045278c3fdc9719eb39207565f45332
joereger <joereger@d42e49d7-e3ba-4d4b-9865-d14956a61029>
2006-02-18 16:56:22 +0000

    Spell Check tweaked.

--- source/WEB-INF/classes/reger/systemproperties/SpellingDictionaryFile.java
+++ source/WEB-INF/classes/reger/systemproperties/SpellingDictionaryFile.java
@@ -7,7 +7,7 @@ public class SpellingDictionaryFile extends SystemProperty{
 
     public SpellingDictionaryFile(){
         setPropertyName("SPELLINGDICTIONARYFILE");
-        setPropertyDefault(WebAppRootDir.getWebAppRootPath() + "spelldict\\dict-large.txt");
+        setPropertyDefault(WebAppRootDir.getWebAppRootPath() + "spelldict"+java.io.File.separator+"dict-large.txt");
         setPropertyDescription("You are able to choose a custom spelling check dictionary file.  The default value you see is for the built-in dictionary.");
         load();
     }
--- source/WEB-INF/classes/reger/systemproperties/SpellingPhoneticFile.java
+++ source/WEB-INF/classes/reger/systemproperties/SpellingPhoneticFile.java
@@ -7,7 +7,7 @@ public class SpellingPhoneticFile extends SystemProperty{
 
     public SpellingPhoneticFile(){
         setPropertyName("SPELLINGPHONETICFILE");
-        setPropertyDefault(WebAppRootDir.getWebAppRootPath() + "spelldict\\phonet.en");
+        setPropertyDefault(WebAppRootDir.getWebAppRootPath() + "spelldict"+java.io.File.separator+"phonet.en");
         setPropertyDescription("You are able to choose a custom spelling check phonetic file.  The default value you see is for the built-in file.");
         load();
     }
